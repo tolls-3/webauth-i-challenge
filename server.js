36ec70b642f1e23d113a267d10a12a100644f86f
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-11-12 19:48:20 +0100

    mvp for day 2 project

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 const express = require("express");
 const server = express();
 const userRouter = require("./users/users");
+const session = require("express-session");
+const connectSessionKnex = require("connect-session-knex");
+const db = require("./data/db-config");
+
+const KnexSessionStore = connectSessionKnex(session)
+const sessionConfig = {
+  name: "rabbit",
+  secret: "keep it secret, keep safe",
+  cookie: {
+    maxAge: 1000 * 30,
+    secure: false,
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialize: false,
+  store: new KnexSessionStore({
+    knex: db,
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  })
+};
+
 server.use(express.json());
+server.use(session(sessionConfig));
 server.use("/api", userRouter);
 
 server.get("/", (req, res) => {
